de9d6b2bba556043f018118673e184b3923e294b
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-08-14 22:18:57 +0430

    Changes added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function page() {
   return (
     <div>
-      <h1 className="text-2xl text-center font-bold">
+      <h1 className="text-2xl text-center text-white font-serif font-bold">
         Welcome to the Home Page
       </h1>
     </div>
