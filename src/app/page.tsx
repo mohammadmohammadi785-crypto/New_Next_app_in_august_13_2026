4f98d1c31cc0c837e240121c21d77f01830e3a3c
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-08-16 14:49:05 +0430

    Changes added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,7 @@ import { User, Code, Globe } from "lucide-react";
 function Home() {
   return (
     <section id="home">
-      <h2 className="text-3xl gradient-bg sm:text-4xl md:text-1xl font-bold mb-4 ">
-        Welcome to My Portfolio
-      </h2>
+      <h2 className="text-3xl">This is the Home Page</h2>
     </section>
   );
 }
