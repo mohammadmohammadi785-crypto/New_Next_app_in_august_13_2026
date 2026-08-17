a4aca8ef4aebe9f4b0237f5bf310bc3c6803213a
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-08-17 17:52:27 +0430

    Changes added

---
+++ src/app/(root)/contact/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <h1 className="text-3xl">This is the contact Page</h1>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,15 +22,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={`h-full antialiased`}>
-      <body className="min-h-full flex flex-col">
-        <header className="w-full mx-auto text-center py-4 text-2xl text-white bg-gray-800">
-          <h1>header</h1>
-        </header>
-        {children}
-        <footer className="w-full text-2xl text-white py-4 text-center bg-gray-900">
-          <h1>footer</h1>
-        </footer>
-      </body>
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
