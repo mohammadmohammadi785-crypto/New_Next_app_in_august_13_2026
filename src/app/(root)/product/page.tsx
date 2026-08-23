7ed72f35c1d695270f89f4b91bb90cb205bae0ac
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-08-23 19:15:21 +0430

    Changes added

--- src/app/(root)/product/page.tsx
+++ src/app/(root)/product/page.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { listPriducts } from "@/db/product";
 import { singleProduct } from "@/types/product.type";
 import ProductCard from "@/components/Product-Card";
 
 export default function page() {
+  const [value, setValue] = useState(1);
   return (
     <div className="w-full max-w-6xl mx-auto my-4 grid md:grid-cols-3 gap-3 grid-cols-1">
       {listPriducts.map((pro: singleProduct) => {
