357718321c85dc10e7b255253fb416ecd0f1af77
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-08-26 20:08:43 +0430

    Changes added

--- src/app/(root)/posts/[id]/[...slug]/page.tsx
+++ src/app/(root)/posts/[id]/[...slug]/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function page() {
   return (
-    <div>
+    <div className="mt-19 h-screen">
       <h1>this is the route that can manage all the segements</h1>
     </div>
   );
