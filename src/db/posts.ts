79bb1f75b2bd3ea9d64050c46713a7a6e086e36c
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-08-26 19:09:57 +0430

    Changes added

---
+++ src/db/posts.ts
@@ -0,0 +1,32 @@
+const posts = [
+  {
+    id: 1,
+    title: "post 1",
+    body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, excepturi consequatur veritatis quibusdam natus tenetur distinctio minima eaque ipsum exercitationem sit sapiente voluptate, temporibus eveniet! Facere atque consequuntur adipisci nostrum.",
+  },
+  {
+    id: 2,
+    title: "post 2",
+    body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, excepturi consequatur veritatis quibusdam natus tenetur distinctio minima eaque ipsum exercitationem sit sapiente voluptate, temporibus eveniet! Facere atque consequuntur adipisci nostrum.",
+  },
+  {
+    id: 3,
+    title: "post 3",
+    body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, excepturi consequatur veritatis quibusdam natus tenetur distinctio minima eaque ipsum exercitationem sit sapiente voluptate, temporibus eveniet! Facere atque consequuntur adipisci nostrum.",
+  },
+  {
+    id: 4,
+    title: "post 4",
+    body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, excepturi consequatur veritatis quibusdam natus tenetur distinctio minima eaque ipsum exercitationem sit sapiente voluptate, temporibus eveniet! Facere atque consequuntur adipisci nostrum.",
+  },
+  {
+    id: 5,
+    title: "post 5",
+    body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, excepturi consequatur veritatis quibusdam natus tenetur distinctio minima eaque ipsum exercitationem sit sapiente voluptate, temporibus eveniet! Facere atque consequuntur adipisci nostrum.",
+  },
+  {
+    id: 6,
+    title: "post 6",
+    body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, excepturi consequatur veritatis quibusdam natus tenetur distinctio minima eaque ipsum exercitationem sit sapiente voluptate, temporibus eveniet! Facere atque consequuntur adipisci nostrum.",
+  },
+];
