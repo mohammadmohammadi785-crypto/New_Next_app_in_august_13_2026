04b899d898b21e7a476455d9a4ca89a4f08a29be
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-08-27 19:54:04 +0430

    Changes added

--- src/app/(root)/product/[product_id]/not-found.tsx
+++ src/app/(root)/product/[product_id]/not-found.tsx
@@ -1,12 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
 export default function NotFoundforProductDeatils() {
   return (
     <div>
-      <h1>
-        the product you are loacking for is not found , please make sure you
+      <h1 className="text-xl">
+        The product you are loacking for is not found, please make sure you
         inserted the id correctly
       </h1>
+      <button>
+        <Link href="/product">Go Back</Link>
+      </button>
     </div>
   );
 }
