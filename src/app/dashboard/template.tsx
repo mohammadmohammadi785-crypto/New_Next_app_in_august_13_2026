d70f1471ffd808701efe15dc1ae5ce490e1533d1
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-09-06 21:16:32 +0430

    Changes added

--- src/app/dashboard/template.tsx
+++ src/app/dashboard/template.tsx
@@ -1,11 +1,26 @@
 import Counter from "@/components/Counter";
 import React from "react";
 
-function DashboardLayout({ children }: { children: React.ReactNode }) {
+function DashboardLayout({
+  children,
+  client,
+  post,
+  product,
+}: {
+  children: React.ReactNode;
+  client: React.ReactNode;
+  post: React.ReactNode;
+  product: React.ReactNode;
+}) {
   return (
     <div>
       <Counter />
-      <div>{children}</div>
+      <div>
+        {children}
+        <div className="w-full max-w-6xl mx-auto grid grid-cols-3 gap-3">
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 }
