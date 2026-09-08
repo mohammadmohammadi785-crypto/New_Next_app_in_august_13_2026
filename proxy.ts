092812f2bc4a2131136da7795c30fc6650811e43
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-09-08 19:36:27 +0430

    Changes added

--- proxy.ts
+++ proxy.ts
@@ -2,12 +2,9 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export function proxy(request: NextRequest) {
-  if (request.nextUrl.pathname == "/product") {
     return NextResponse.redirect(new URL("/", request.nextUrl));
-  }
-  return NextResponse.next();
 }
 
-// export const config = {
-//   matcher: "/product",
-// };
+export const config = {
+  matcher: "/product",
+};
