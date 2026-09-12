2decd758214e5abe580f149f648d6efdd95a176e
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-09-12 22:13:05 +0430

    Changes added

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function notfound() {
-  return <div></div>;
+  return (
+    <div className="w-full flex justify-center items-center h-screen">
+      <h1>Please wait</h1>
+    </div>
+  );
 }
