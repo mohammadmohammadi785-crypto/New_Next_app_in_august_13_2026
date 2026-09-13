9325281d9e9734f85269fa2288994279792275ac
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-09-13 21:16:13 +0430

    Changes added

---
+++ src/app/(root)/product/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function loading() {
+  return (
+    <div className="w-full flex justify-center items-center h-screen">
+      <h1>loading product</h1>
+    </div>
+  );
+}
+
+export default loading;
