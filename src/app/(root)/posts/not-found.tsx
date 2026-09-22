12911130d4940afa88768b9e718abcbd5de2302b
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-09-22 19:04:31 +0430

    End Commit

--- src/app/(root)/posts/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function notfound() {
-  return (
-    <div className="w-full flex justify-center items-center">
-      <h2>post was Not found </h2>
-    </div>
-  );
-}
