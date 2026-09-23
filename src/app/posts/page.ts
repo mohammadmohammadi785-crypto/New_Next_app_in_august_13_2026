471838860a1ad20d3fead46af26a1bc674e4cf66
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-09-23 19:17:13 +0430

    Changes added

---
+++ src/app/posts/page.ts
@@ -0,0 +1,12 @@
+export const comments = [
+  {
+    id: 1,
+    title: "This is the comment 1",
+    body: "This is the body of comment 1",
+  },
+  {
+    id: 2,
+    title: "This is the comment 2",
+    body: "This is the body of comment 2",
+  },
+];
