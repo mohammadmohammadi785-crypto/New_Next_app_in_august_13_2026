e88d2f02aa7c6b7945f91044eb5e63c63fc849ce
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-09-23 19:10:17 +0430

    End Commit

---
+++ src/app/posts/route.ts
@@ -0,0 +1,17 @@
+export async function GET() {
+  const response = [
+    {
+      id: 1,
+      title: "This is the post 1",
+    },
+    {
+      id: 2,
+      title: "This is the post 2",
+    },
+    {
+      id: 3,
+      title: "This is the post 3",
+    },
+  ];
+  const data = response.join();
+}
