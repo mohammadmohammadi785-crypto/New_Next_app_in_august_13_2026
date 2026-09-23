4a4dfea4ace493595b49fbda9d5479586256f4fe
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-09-23 19:25:57 +0430

    Changes added

--- src/app/posts/route.ts
+++ src/app/posts/route.ts
@@ -1,7 +1,22 @@
+import { comments } from "./comments";
+
 type Comment = {
   id: number;
   title: string;
   body: string;
 };
 
-export function GET() {}
+export function GET() {
+  return Response.json(comments);
+}
+
+export async function POST(request: Request) {
+  const newrequest = await request.json();
+  const newComment = {
+    id: comments.length + 1,
+    title: newrequest.title,
+    body: newrequest.body,
+  };
+  comments.push(newComment);
+  return Response.json(newComment);
+}
